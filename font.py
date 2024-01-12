48a81c5291396bce37790e0435baaf2e3d22d1bc
TreXHere <105743848+TreXHere@users.noreply.github.com>
2024-01-12 11:10:35 +0530

    Update font.py

--- font.py
+++ font.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message
 
 # Replace 'YOUR_API_ID' and 'YOUR_API_HASH' with your actual API credentials
 api_id = '21364355'
-api_hash = '72f11aec1dd3e5764554d477341a3d0b"'
+api_hash = '72f11aec1dd3e5764554d477341a3d0b'
 bot_token = '6337104990:AAFxTIVSr3BtFxWEossUWZRbQIY4Dac8J4U'
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
@@ -17,5 +17,10 @@ async def delete_font_messages(client: Client, message: Message):
         # Reply that fonts are not allowed
         await message.reply_text("Fonts are not allowed in this group.")
 
+@app.on_message(filters.group & filters.edited)
+async def delete_edited_messages(client: Client, message: Message):
+    # Delete the edited message
+    await message.delete()
+    await message.reply_text("No Message Editing") 
 
 app.run()
