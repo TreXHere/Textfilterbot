6a12d73c47d564e8fe48e1f42f0f2b7d3823f7e6
TreXHere <105743848+TreXHere@users.noreply.github.com>
2024-01-12 12:05:44 +0530

    Update font.py

--- font.py
+++ font.py
@@ -1,22 +1,22 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from edit import *
 
 # Replace 'YOUR_API_ID' and 'YOUR_API_HASH' with your actual API credentials
 api_id = '21364355'
 api_hash = '72f11aec1dd3e5764554d477341a3d0b'
 bot_token = '6337104990:AAFxTIVSr3BtFxWEossUWZRbQIY4Dac8J4U'
 
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+app = Client("bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
+ADMINS = [6574111464, 1600454750, 609517172, 2122515260]
 
-@app.on_message(filters.group & filters.text & filters.administrators)
+@app.on_message(filters.group & filters.text & filters.user(ADMINS))
 async def delete_font_messages(client: Client, message: Message):
     if message.text and not message.text.isascii():
         # If the message contains non-ASCII characters (indicating it's a font), delete it
         await message.delete()
         # Reply that fonts are not allowed
-        await message.reply_text("Fonts are not allowed in this group.")
+        await message.reply_text("BossNup Ultra Security No Font text allowed")
 
 
 app.run()
