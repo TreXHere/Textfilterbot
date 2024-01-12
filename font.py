ead84282a88aff0ad331920004d5c69cb5875149
TreXHere <105743848+TreXHere@users.noreply.github.com>
2024-01-12 13:04:44 +0530

    Update font.py

--- font.py
+++ font.py
@@ -1,15 +1,26 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from config import app 
+
+# Replace 'YOUR_API_ID' and 'YOUR_API_HASH' with your actual API credentials
+api_id = '21364355'
+api_hash = '72f11aec1dd3e5764554d477341a3d0b'
+bot_token = '6337104990:AAFxTIVSr3BtFxWEossUWZRbQIY4Dac8J4U'
+
+app = Client("bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 ADMINS = [6574111464, 1600454750, 609517172, 2122515260]
 
-@app.on_message(filters.group & filters.text & filters.user(ADMINS))
+def contains_font(text):
+    # Check if the message contains non-ASCII characters (indicating it's a font)
+    return not all(char.isascii() or char in (' ', '\n') for char in text)
+
+
+@app.on_message(filters.group & filters.text & ~filters.user(app.me))
 async def delete_font_messages(client: Client, message: Message):
-    if message.text and not message.text.isascii():
+    if contains_font(message.text):
         # If the message contains non-ASCII characters (indicating it's a font), delete it
         await message.delete()
-        # Reply that fonts are not allowed
-        await message.reply_text("BossNup Ultra Security No Font text allowed")
+        # Reply and mention the sender    
+        await message.reply_text(f"**Font Deleted**")
 
-
+app.run()
