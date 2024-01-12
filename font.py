cca0ceb4cf0981b9b1cc6d620a4eb00c098b9ef8
TreXHere <105743848+TreXHere@users.noreply.github.com>
2024-01-12 12:07:27 +0530

    Update font.py

--- font.py
+++ font.py
@@ -1,12 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-
-# Replace 'YOUR_API_ID' and 'YOUR_API_HASH' with your actual API credentials
-api_id = '21364355'
-api_hash = '72f11aec1dd3e5764554d477341a3d0b'
-bot_token = '6337104990:AAFxTIVSr3BtFxWEossUWZRbQIY4Dac8J4U'
-
-app = Client("bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+from config import app 
 
 ADMINS = [6574111464, 1600454750, 609517172, 2122515260]
 
@@ -19,4 +13,3 @@ async def delete_font_messages(client: Client, message: Message):
         await message.reply_text("BossNup Ultra Security No Font text allowed")
 
 
-app.run()
