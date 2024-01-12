88eea70956b173bc8380efdeac72f5e1c90f0e76
TreXHere <105743848+TreXHere@users.noreply.github.com>
2024-01-12 12:08:48 +0530

    Update edit.py

--- edit.py
+++ edit.py
@@ -1,8 +1,9 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from config import app
 
 @app.on_message(filters.group & filters.edited)
 async def delete_edited_messages(client: Client, message: Message):
     # Delete the edited message
     await message.delete()
-    await message.reply_text("Editing Message Deleted") 
+    await message.reply_text("Edit Deleted") 
